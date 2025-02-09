5cc37e6b35311f04133f8da5f6855c4d4d51f87c
psjw <dlrudtn108@gmail.com>
2025-02-09 19:38:56 +0900

    first commit

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,17 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        @Override
+        public void run() /*throws Exception*/ { //주석 풀면 예외발생
+//            throw new Exception(); //주석 풀면 예외발생
+        }
+    }
+}
